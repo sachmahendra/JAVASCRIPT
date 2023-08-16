7d28c53097eaf2fec2239e7795b574229c7c337a
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-08-16 11:25:19 +0530

    initial.commit
    IT IS A JAVASCRIPT REPOSITORY IN WHICH WE WILL SEE JS CODE

---
+++ L2TypeConversion.js
@@ -0,0 +1,17 @@
+let a = "hello";
+
+console.log(a);
+console.log(typeof(a));
+console.log(typeof Number(a));
+
+// use of toString 
+
+let c = 54;
+//c.toString();
+c = c.toString();
+console.log(c);
+console.log(typeof c);
+
+// all operation are same on different data type like,replace, substrig , slicing, read
+
+// DOM MANUPLATION
---
+++ index.js
@@ -0,0 +1,23 @@
+let x = 10;
+
+let y = "hello";
+let z = 50;
+
+console.log(x);
+console.log(typeof x);
+
+console.log(y);
+console.log(typeof y);
+
+let a = true;
+
+console.log(x + z + a);
+
+
+
+let name = 'mahi';
+let surname = "yadav";
+
+console.log("hey, my name is " + name + " " + surname + ".");
+
+console.log('Hey, my name is ${name} ${surname}.');
