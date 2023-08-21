9fe3c61a7009a964abb1e1c6990f26513022a442
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-08-21 19:37:57 +0530

    initial.commit
    IT IS A JAVA SCRIPT CODE FOR THE IMPLEMENTATION OF ARRAY

---
+++ L17Array.js
@@ -0,0 +1,19 @@
+// let book = ["Math", "physics", "bio", "computer"];
+// let booklength = book,
+//     length;
+
+// for (i = 0; i < booklength; i++) {
+//     //console.log('element ${i} is ${ook[i]} \n');
+//     console.log(book[i]);
+// }
+
+function loopArray() {
+    let arr = ['Item 1', 'Item 2', 'Item 3',
+        'Item 4', 'Item 5'
+    ];
+
+    for (i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+loopArray();
