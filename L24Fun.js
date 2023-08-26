47f655dd15e0c88d579e43fc0d3ced06d0ae0442
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-08-26 22:55:24 +0530

    initial.commit
    IT IS A JAVASCRIPT CODE FOR THE IMPLEMENTATION OF FUNCTION

--- L24Fun.js
+++ L24Fun.js
@@ -1,9 +1,13 @@
+// anonymous function
+
 // let callfu = function() {
 //     console.log("hello mahi0");
 
 // };
 // callfu();
 
+
+// this is immediately invoked function
 (function() {
     console.log("hello mahi0");
 
